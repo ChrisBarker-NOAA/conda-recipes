f913107caeee7652292ffb22afca98fbd5134d3d
chris.barker <chris.barker@noaa.gov>
2015-03-05 08:47:57 -0800

    re-added the conditional build of the configure script in udunits2/build.sh
    This will let the same build.sh work regardless of whether the source was
    pulled form the offical tarball or github repo
    (that gets set in meta.yaml)

--- udunits2/build.sh
+++ udunits2/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-## should have a configure file if you got the offical source tarball
-# if [ ! -f configure ];
-# then
-#    # Make the configure file. Need autoreconf, libtool, libexpat-dev for this.
-#    autoreconf -i --force
-# fi
+# should have a configure file if you got the offical source tarball
+# but if it got pulled from the git repo, the configure script needs to be 
+# rebuilt
+if [ ! -f configure ];
+then
+   # Make the configure file. Need autoreconf, libtool, libexpat-dev for this.
+   autoreconf -i --force
+fi
 
 ./configure --prefix=$PREFIX
 make
